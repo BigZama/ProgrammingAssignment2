bdccfc6dba60c991a00cbe6c2268b0370f783517
Zama <zamanillodiaz@gmail.com>
2018-07-20 10:10:51 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,9 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Week 3 Assignment. GitHub user - Big_Zama
 
+## The function below creates a matrix object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
         inver <- NULL
         set <- function(y) {
@@ -18,8 +19,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
+## This function calculates the inverse of the previous matrix returned by makeCacheMatrix function
+## If the inverse has already been calculated, then cacheSolve will retrieve the inverse from the cache
 cachesolve <-function(x, ...) {
         inver <- x$getinver()
         if(!is.null(inver)) {
@@ -31,3 +32,34 @@ cachesolve <-function(x, ...) {
         x$setinver(inver)
         inver
 }
+
+
+## -- Testing with Alan E. Berger data (posted in the course forum) --
+> m1 <- matrix(c(1/2, -1/4, -1, 3/4), nrow = 2, ncol = 2)
+> m1
+      [,1]  [,2]
+[1,]  0.50 -1.00
+[2,] -0.25  0.75
+> I2 <- matrix(c(1,0,0,1), nrow = 2, ncol = 2)
+> n1 <- matrix(c(6,2,8,4), nrow = 2, ncol = 2)
+> m1 %*% n1
+     [,1] [,2]
+[1,]    1    0
+[2,]    0    1
+> I2 == m1 %*% n1   # I was reviewing previous lessons
+     [,1] [,2]
+[1,] TRUE TRUE
+[2,] TRUE TRUE
+> solve(n1)
+      [,1]  [,2]
+[1,]  0.50 -1.00
+[2,] -0.25  0.75
+> myMatrix_object <- makeCacheMatrix(m1)
+> cacheSolve(myMatrix_object)           # Case sensitive mistake :P
+Error in cacheSolve(myMatrix_object) : 
+  could not find function "cacheSolve"
+> cachesolve(myMatrix_object)
+     [,1] [,2]
+[1,]    6    8
+[2,]    2    4
+> 
